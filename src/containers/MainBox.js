44c3f4df26d14b0941ec942a86ec6f6d8db1cfbe
lesliesage <leslie.sage@gmail.com>
2019-08-29 11:04:39 -0400

    Done.

--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -3,22 +3,57 @@ import MenuBar from '../components/MenuBar.js'
 import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {pageToShow: "profile"}
+  }
 
+  activateProfile = () => {
+    this.setState({pageToShow: "profile"})
+  }
+
+  activatePhoto = () => {
+    this.setState({pageToShow: "photo"})
+  }
+
+  activateCocktail = () => {
+    this.setState({pageToShow: "cocktail"})
+  }
+
+  activatePokemon = () => {
+    this.setState({pageToShow: "pokemon"})
+  }
+
+  detailsToDisplay = () => {
+    switch (this.state.pageToShow) {
+      case "photo":
+        return <div>{Photos()}</div>;
+      case "cocktail":
+        return <div>{Cocktails()}</div>;
+      case "pokemon":
+        return <div><Pokemon /></div>;
+      default:
+        return <div>{Profile()}</div>;
+    }
+  }
 
   render() {
-
     /*
-
-    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
-
+    Replace the code below! Depending on what menu item is selected in the menu, 
+    I should render either a Profile, Photos, Cocktails, or Pokemon component.
+    Think of a way to track which menu item was selected. Which component should 
+    have state? Which component should have methods to control state? Where should 
+    these methods be called?
     */
-
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
-
     return (
       <div>
-        <MenuBar />
-        {detailsToDisplay}
+        <MenuBar 
+          activateProfile={this.activateProfile}
+          activatePhoto={this.activatePhoto}
+          activateCocktail={this.activateCocktail}
+          activatePokemon={this.activatePokemon}
+        />
+        {this.detailsToDisplay()}
       </div>
     )
   }
